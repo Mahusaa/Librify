9d5fb65dc065f37bcaef2fe11bb9e96ac958cb71
Mahusaa <usamah.hafizh@ui.ac.id>
2024-06-28 17:32:40 +0700

    modify podcast to fix deploy

--- src/app/podcast/page.tsx
+++ src/app/podcast/page.tsx
@@ -1,10 +1,13 @@
-import MenuWrapper from "~/components/MenuWrapper";
 
 export default function PodcastPage() {
   return (
-    <MenuWrapper label="Podcast">
-      child
-    </MenuWrapper>
+    <>
+
+      <span className="font-bold text-4xl">Messages</span>
+
+      <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+    </>
   )
 }
 
