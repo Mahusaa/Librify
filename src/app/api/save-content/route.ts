454b4c9dd03f3c10f7996aa3c1d6e19c8a2827e3
Mahusaa <usamah.hafizh@ui.ac.id>
2024-07-15 18:20:00 +0700

    ref: add interface to route

--- src/app/api/save-content/route.ts
+++ src/app/api/save-content/route.ts
@@ -1,8 +1,12 @@
 import { eq } from "drizzle-orm";
 import { db } from "~/server/db";
 import { chapter } from "~/server/db/schema";
+interface RequestBody {
+  chapterId: string;
+  content: string;
+}
 export async function POST(req: Request) {
-  const { chapterId, content } = await req.json();
+  const { chapterId, content }: RequestBody = await req.json() as RequestBody;
   try {
     await db.update(chapter).set({ content }).where(eq(chapter.id, chapterId))
     console.log("updating..")
