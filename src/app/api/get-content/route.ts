21825852613742731ee7a4aafc0ebaec41846aec
Mahusaa <usamah.hafizh@ui.ac.id>
2024-07-21 18:42:17 +0700

    feat: add get content and delete modal

---
+++ src/app/api/get-content/route.ts
@@ -0,0 +1,11 @@
+import { db } from "~/server/db";
+
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const id = url.searchParams.get("id") as string;
+  const res = await db.query.chapter.findFirst({
+    where: (model, { eq }) => eq(model.id, id)
+  })
+
+  return Response.json(res);
+}
