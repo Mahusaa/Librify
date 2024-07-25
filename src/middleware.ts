0cbd84af1493771109367b3321671cdaf0e61506
Mahusaa <usamah.hafizh@ui.ac.id>
2024-07-25 21:28:27 +0700

    fix: cant open books page resolved

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
   const session = !!req.cookies.get("next-auth.session-token")
 
   if (!session) {
-    return NextResponse.redirect(new URL(`/sign-in`, req.url));
+    return NextResponse.redirect(new URL(`/api/auth/signin?callbackUrl=${path}`, req.url));
   }
   return NextResponse.next();
 }
